e215de4a3b86da1e00b732d2673d307fc753d68b
nirmala <nirmala@surf.net.np>
2018-12-22 22:07:16 +0100

    add snowflake component and background image

---
+++ src/Snowflake.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SnowFlake = () => {
+  return (
+    <div>
+      <div className="snowflakes" aria-hidden="true">
+        <div className="snowflake">❅</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❅</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❅</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❅</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❅</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❅</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❆</div>
+        <div className="snowflake">❆</div>
+      </div>
+    </div>
+  );
+};
+
+export default SnowFlake;
