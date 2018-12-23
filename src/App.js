8b1feaa075bf3487485058f2a0c9a62ab14cfc2d
nirmala <nirmala@surf.net.np>
2018-12-23 21:46:19 +0100

    add child component

--- src/App.js
+++ src/App.js
@@ -1,88 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
+import Clock from "./Clock";
 import "./App.css";
 import SnowFlake from "./Snowflake";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      days: 0,
-      hours: 0,
-      min: 0,
-      sec: 0
-    };
-    this.calculateRemainingDays = this.calculateRemainingDays.bind(this);
-  }
-  componentDidMount() {
-    // update every second
-    this.interval = setInterval(() => {
-      const date = this.calculateRemainingDays(this.props.date);
-      date ? this.setState(date) : this.stop();
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    this.stop();
-  }
-
-  calculateRemainingDays() {
-    let endDate = Date.parse("Dec 24 2018");
-    let currentDate = Date.now();
-    let remainingTime = endDate - currentDate;
-    let days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
-    let minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
-    let seconds = Math.floor((remainingTime / 1000) % 60);
-    return {
-      days: days,
-      hours: hours,
-      min: minutes,
-      sec: seconds
-    };
-  }
-  stop() {
-    clearInterval(this.interval);
-  }
-  addLeadingZeros(value) {
-    value = String(value);
-    while (value.length < 2) {
-      value = "0" + value;
-    }
-    return value;
-  }
-
-  render() {
-    const countDown = this.state;
-    return (
-      <div className="App">
-        <div className="background">
-          <h1>Hack your Future</h1>
-          <h3>2018 Christmas Edition</h3>
-          <span className="col">
-            <strong>{this.addLeadingZeros(countDown.days)}</strong>
-            Days
-          </span>
-          <span className="col">
-            <strong> {this.addLeadingZeros(countDown.hours)}</strong>
-            hours
-          </span>
-          <span className="col">
-            <strong>{this.addLeadingZeros(countDown.min)}</strong>
-            minutes
-          </span>
-          <span>
-            <strong>{this.addLeadingZeros(countDown.sec)}</strong>
-            seconds
-          </span>
-          <SnowFlake />
-          <footer className="footer">
-            <p>Background Photo by Daniel Reche from Pexels</p>
-          </footer>
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="background">
+        <h1>Hack your Future</h1>
+        <h3>2018 Christmas Edition</h3>
+        <Clock
+          finalTime={Date.parse("Dec 24 2018")}
+          message="Merry Christmas!"
+        />
+        <SnowFlake />
+        {/*         <Clock
+          finalTime={Date.parse("Jan 1 2019")}
+          message="Happy New Year 2019!"
+        />
+ */}{" "}
+        <footer className="footer">
+          <p>Background Photo by Daniel Reche from Pexels</p>
+        </footer>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
